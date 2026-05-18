8613201fd17dd71d9ef706e7b2dbe2efec3e2a10
Yii-de <totuanhuy24082005@gmail.com>
2026-05-18 14:43:57 +0700

    style(01-01): implement Deep Blue branding and theme
    - Defined DeepBlue primary, secondary, and tertiary colors
    - Updated Material 3 color schemes
    - Renamed theme to AndroidOCRTheme
    - Disabled dynamic colors by default to respect brand identity

--- app/src/main/java/com/t2h/ocr/MainActivity.kt
+++ app/src/main/java/com/t2h/ocr/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.t2h.ocr.ui.theme.FrontendAndroidOCRTheme
+import com.t2h.ocr.ui.theme.AndroidOCRTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FrontendAndroidOCRTheme {
+            AndroidOCRTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    FrontendAndroidOCRTheme {
+    AndroidOCRTheme {
         Greeting("Android")
     }
 }
