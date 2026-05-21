3db882f97fb0cb1f3f5c9a4b09d54e28c4e73604
Yii-de <totuanhuy24082005@gmail.com>
2026-05-21 17:18:03 +0700

    feat(02-01): add Firebase and WorkManager dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.androidx.work.runtime.ktx)
+    implementation(libs.play.services.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
